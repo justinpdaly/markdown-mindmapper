a53d34659a70c7a3f6f2c45d821313ff249ad8c6
justinpdaly <42853630+justinpdaly@users.noreply.github.com>
2024-11-28 11:54:57 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { Card } from '@/components/ui/Card';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,5 +9,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/markdown-mindmapper/', // Change this to your repo name for GitHub Pages
 });

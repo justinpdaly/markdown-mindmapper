16e864fec6637a86a1e83c5264f87c0ddc9691bd
justinpdaly <42853630+justinpdaly@users.noreply.github.com>
2024-11-28 11:37:23 +1100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarkdownMindMapper from './components/MarkdownMindMapper';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <MarkdownMindMapper />
+    </div>
+  );
+}
+
+export default App;
